2662a1cc50f1a3dd4bc754a2671263c4e69eeff3
paul <paulalexandre.fortuna@docaret.com>
2025-03-26 01:27:13 +0100

    feat(mongo): migrated the whole pokemon base from json file to mongo

---
+++ src/config/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      dbName: "pokemon",
+    });
+    console.log(`MongoDB connecté: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Erreur de connexion à MongoDB: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
